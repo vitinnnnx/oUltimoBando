ee5c41ecda8dba4b9774119166a8b8f42d1ca751
Victor Felipe <vitinhofelipe33@gmail.com>
2025-11-17 15:11:27 -0300

    base do jogo e layout da primeira fase

---
+++ scripts/player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -500.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if direction > 0:
+		anim.flip_h = false
+		anim.play("run")
+	elif direction < 0:
+		anim.flip_h = true
+		anim.play("run")
+	else:
+		anim.play("idle")
+
+	move_and_slide()
