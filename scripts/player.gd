0a350305b518eb971b10977e9b0f6000fbb4e913
Victor Felipe <vitinhofelipe33@gmail.com>
2025-11-25 22:48:42 -0300

    maquina de estado e suavizacao de movimentos

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,34 +1,99 @@
 extends CharacterBody2D
+
+
+enum PlayerState {
+	idle,
+	walk,
+	jump,
+}
+
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 300.0
+@export var max_speed = 300.0
+@export var aceleration = 100
+@export var deceleration = 100
 const JUMP_VELOCITY = -500.0
 
+var direction = 0
+var status: PlayerState
+
+func move(delta):
+	update_direction()
+
+	if direction:
+		velocity.x = move_toward(velocity.x, direction * max_speed, aceleration * delta)
+	else:
+		velocity.x = move_toward(velocity.x, direction  * max_speed, deceleration * delta)
+
+func _ready() -> void:
+	go_to_idle_state()
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	if direction > 0:
-		anim.flip_h = false
-		anim.play("run")
-	elif direction < 0:
-		anim.flip_h = true
-		anim.play("run")
-	else:
-		anim.play("idle")
-
+	
+	match status: 
+		PlayerState.idle:
+			idle_state(delta)
+			anim.play("idle")
+		PlayerState.walk:
+			walk_state(delta)
+			anim.play("walk")
+		PlayerState.jump:
+			jump_state(delta)
+			
+			
 	move_and_slide()
+
+
+func go_to_idle_state():
+	status = PlayerState.idle
+	anim.play("idle")
+func go_to_walk_state():
+	status = PlayerState.walk
+	anim.play("walk")
+func go_to_jump_state():
+	status = PlayerState.jump
+	velocity.y = JUMP_VELOCITY
+
+func idle_state(delta):
+	move(delta)
+	if velocity.x != 0:
+		go_to_walk_state()
+		return
+		
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+
+func jump_state(delta):
+	move(delta)
+	if is_on_floor():
+		if velocity.x == 0:
+			go_to_idle_state()
+		else: 
+			go_to_walk_state()
+		return
+		
+func walk_state(delta ):
+	move(delta)
+	if velocity.x == 0:
+		go_to_idle_state()
+		return
+		
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+
+
+
+
+func update_direction():
+	direction = Input.get_axis("left", "right")
+	
+	if direction < 0:
+		anim.flip_h = true
+	elif direction > 0:
+		anim.flip_h = false
+		
